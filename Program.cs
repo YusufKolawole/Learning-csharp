4d44eb3f49099927c517dc2c471e80dce34be9fa
Yusuf Quadri Kolawole <quadyusuf@gmail.com>
2024-09-17 09:15:30 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿Console.WriteLine("Hello, World!\nit's working");
-Console.WriteLine("This is awesome " + DateTime.Now);
+﻿double heightInMetres = 1.88;
+WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}.");
+string name = "Kolawole";
+
+WriteLine(nameof(name));
+WriteLine(name);
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b61f55d02b3ad7020a519ce457321e73ef1ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
